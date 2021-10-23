e4329b726977b5b646ed8eeee831f8d819bc2e71
caner.demirci <caner.demirci@trendyol.com>
2021-10-24 00:00:22 +0300

    add car model.

---
+++ sample/Model/Car.cs
@@ -0,0 +1,10 @@
+namespace SpecificationSampleApi.Model
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+    }
+}
